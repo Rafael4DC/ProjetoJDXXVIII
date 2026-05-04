d68b6383fb28db717efd4aaa1a06b0a6b35ccccd
Ze das couves <naosei@gmail.com>
2026-05-04 19:50:11 +0100

    Initial commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjetoJDXXVIIIModel;
+using ProjetoJDXXVIIIServices;
+namespace ProjetoJDXXVIII.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        internal List<Supplier> Ids = new List<Supplier>();
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Ids = BaseServices.GetSupplierIds();
+        }
+    }
+
+}
